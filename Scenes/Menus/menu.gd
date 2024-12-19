a07b303cef38e02d05e1475e93cb3bfe01744324
Liam J. <liam@jewer.ca>
2024-12-19 17:43:40 -0500

    added menu graphics

--- Scenes/Menus/menu.gd
+++ Scenes/Menus/menu.gd
@@ -1,6 +1,9 @@
 class_name Menu
 extends Control
-
+	
+@onready var start_button: TextureButton = $HBoxContainer/VBoxContainer/StartButton as TextureButton
+@onready var quit_button: TextureButton = $HBoxContainer/VBoxContainer/QuitButton as TextureButton
+@onready var start_level = preload("res://Scenes/Levels/Test.tscn") as PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,10 +14,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-@onready var start_button: Button = $VBoxContainer/StartButton as Button
-@onready var quit_button: Button = $VBoxContainer/QuitButton as Button
-@onready var start_level = preload("res://Scenes/Levels/Test.tscn") as PackedScene
 
 func on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(start_level)
